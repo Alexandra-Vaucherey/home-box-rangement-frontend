241612f0bc18de0d4f9dbee2a72c7fa4ea88c83f
Alexandra <shivaotaku@gmail.com>
2024-04-07 16:42:41 +0200

    welcome page

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -7,14 +7,18 @@ import FontAwesome from 'react-native-vector-icons/FontAwesome';
 export default function ProfileScreen({ navigation }) {
  return (
    <View style= {styles.container}>
-    <View>
-      <FontAwesome name='trash-o' />
-    </View>
+    <View style= {styles.header}>
+      <FontAwesome name='home' size ={44} color = "#084364" style={{marginRight: 20}}/>
      <Text style= {styles.title}>Bienvenue Utilisateur</Text>
+      <FontAwesome name='gear' size ={44} color = "#084364" style={{marginLeft: 20}}/>
+    </View>
      <Text style={styles.text}>Veuillez sélectionner vos habitats et dépendances</Text>
      <TouchableOpacity style= {styles.home}> 
-     <Text>Maison principale</Text>
-
+     <FontAwesome name='home' size={30} color={'#084364'} style={{marginRight: 20}}/>
+     <Text style={styles.homeText}>Maison {'\n'}principale</Text>
+     </TouchableOpacity>
+     <TouchableOpacity style={styles.add}>
+      <FontAwesome name='plus' size={30} color={'#084364'}/>
      </TouchableOpacity>
      <Button
        title="Go to Home"
@@ -32,6 +36,13 @@ const styles = StyleSheet.create({
       justifyContent: 'space-around',
     },
 
+    header: {
+      display: 'flex',
+      flexDirection: 'row',
+     alignItems: 'center', 
+     justifyContent: 'space-between',
+    },
+
     title: {
         fontSize: 50,
         fontWeight: 'bold',
@@ -40,11 +51,40 @@ const styles = StyleSheet.create({
         borderRadius: 10,
         width: 270,
         textAlign: 'center',
+
     },
 
     text: {
         fontSize: 24,
+        textAlign: 'center',
 
+    },
+
+    home: {
+      display: 'flex',
+      flexDirection: 'row',
+      alignItems: 'center',
+      borderRadius: 10,
+      backgroundColor: 'white',      
+      elevation: 10,
+      height: 50,
+      width: 150,    
+      justifyContent: 'center',
+    },
+
+    homeText: {
+      textAlign: 'center',
+      color: '#084364',
+      fontSize: 18,
+    },
+
+    add: {
+      backgroundColor:  '#66D1FF',
+      height: 40,
+      width: 40,
+      borderRadius: 20,
+      alignItems: 'center',
+      justifyContent: 'center',
     }
   });
   
